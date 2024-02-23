fd5e0f3b81246cf3bb54e7e387c279127fd51317
tto <to1334s@greenwich.ac.uk>
2024-02-23 14:26:25 +0630

    all complete

---
+++ resources/views/ideas/modal.blade.php
@@ -0,0 +1,25 @@
+<div class="remodal" data-remodal-id="remodal" role="dialog" aria-labelledby="modal1Title" aria-describedby="modal1Desc">
+	<button data-remodal-action="close" class="remodal-close" aria-label="Close"></button>
+	<div class="remodal-content">
+		<h2 id="modal1Title">Terms and Conditions</h2>
+		<p id="modal1Desc">
+		Welcome to our website. If you continue to create news feed, you are agreeing to comply with and be bound by the following terms and conditions :
+		</p>
+        <ul>
+            
+            <li>Disclaimer</li>
+            <li>Intellectual Property</li>
+            <li>Third-Party Links</li>
+            <li>Privacy Policy</li>
+            <li>Limitation of Liability</li>
+            <li>respest all</li>
+            <li>Changes to the Terms and Conditions</li>
+        </ul>
+        <p>If you disagree with any part of these terms and conditions, please do not use create
+            <br>
+            <input type="checkbox" id="agree" onclick="enableSave()"> <label for="">Accept Terms & Condition</label>
+        <div class="d-flex justify-content-end my-2">
+            <button type="button" class="btn btn-primary btn-ladda" id="save" disabled onclick="submitForm()">Save</button>
+        </div>
+	</div>
+</div>
